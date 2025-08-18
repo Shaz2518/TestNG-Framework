4375c2bda40949fdff2c61bf468d29a26c0dac76
Shaziya Shaikh <shaziyashaikh2518@outlook.com>
2025-08-18 21:41:34 +0530

    TestNG XML

---
+++ src/TestNG_Basics/CarLoan.java
@@ -0,0 +1,25 @@
+package TestNG_Basics;
+
+import org.testng.annotations.Test;
+
+public class CarLoan {
+	
+	@Test
+	public void webLoginCar()
+	{
+		System.out.println("Web Login Car Loan");
+	}
+	
+	@Test
+	public void mobileLoginCar()
+	{
+		System.out.println("Mobile Login Car Loan");
+	}
+	
+	@Test
+	public void APILoginCar()
+	{
+		System.out.println("API Login Car Loan");
+	}
+
+}
---
+++ src/TestNG_Basics/HomeLoan.java
@@ -0,0 +1,26 @@
+package TestNG_Basics;
+
+import org.testng.annotations.Test;
+
+public class HomeLoan {
+
+	
+	@Test
+	public void webLoginHome()
+	{
+		System.out.println("Web Login Home Loan");
+	}
+	
+	@Test
+	public void mobileLoginHome()
+	{
+		System.out.println("Mobile Login Home Loan");
+	}
+	
+	@Test
+	public void APILoginHome()
+	{
+		System.out.println("API Login Home Loan");
+	}
+
+}
